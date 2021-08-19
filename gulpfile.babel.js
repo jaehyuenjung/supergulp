19e86722012f879719c4e6a94301f051139a324f
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-19 11:21:56 +0900

    Deploying to GH Pages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,7 @@ import autoprefixer from "gulp-autoprefixer";
 import miniCSS from "gulp-csso";
 import bro from "gulp-bro";
 import babelify from "babelify";
+import ghPages from "gulp-gh-pages";
 
 const sass = require("gulp-sass")(require("sass"));
 
@@ -35,7 +36,7 @@ const routes = {
 const pug = () =>
     gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del(["build/"]);
+const clean = () => del(["build/", ".publish"]);
 
 const webserver = () =>
     gulp.src("build").pipe(ws({ livereload: true, open: true }));
@@ -47,7 +48,7 @@ const styles = () =>
     gulp
         .src(routes.scss.src)
         .pipe(sass().on("error", sass.logError))
-        .pipe(autoprefixer({ browsers: ["last 2 versions"] }))
+        .pipe(autoprefixer())
         .pipe(miniCSS())
         .pipe(gulp.dest(routes.scss.dest));
 
@@ -64,6 +65,8 @@ const js = () =>
         )
         .pipe(gulp.dest(routes.js.dest));
 
+const gh = () => gulp.src("build/**/*").pipe(ghPages());
+
 const watch = () => {
     gulp.watch(routes.pug.watch, pug);
     gulp.watch(routes.img.src, img);
@@ -75,6 +78,8 @@ const prepare = gulp.series([clean, img]);
 
 const assets = gulp.series([pug, styles, js]);
 
-const postDev = gulp.parallel([webserver, watch]);
+const live = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets, postDev]);
+export const build = gulp.series([prepare, assets]);
+export const dev = gulp.series([build, live]);
+export const deploy = gulp.series([build, gh, clean]);
